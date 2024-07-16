70604701a2e448035b9cb0c6b4c04de3e5b7796e
dawid mrotek <DawidMrotek@protonmail.ch>
2024-07-17 01:31:15 +0200

    [ADD] detect ball

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8x')
+model = YOLO('models/yolo5_best.pt')
 
 #result = model.predict('input_videos/image.png', save=True)
-result = model.predict('input_videos/input_video.mp4', save=True)
+result = model.predict('input_videos/input_video.mp4', save=True, conf=0.2)
 print(result)
 print("boxes:")
 for box in result[0].boxes:
