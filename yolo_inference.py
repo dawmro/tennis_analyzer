da76fbbde4a638634854a0275a87e935bf28a293
dawid mrotek <DawidMrotek@protonmail.ch>
2024-07-17 02:05:34 +0200

    [ADD] track objects

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,9 +1,10 @@
 from ultralytics import YOLO
 
-model = YOLO('models/yolo5_best.pt')
+#model = YOLO('models/yolo5_best.pt')
+model = YOLO('yolov8x.pt')
 
-#result = model.predict('input_videos/image.png', save=True)
-result = model.predict('input_videos/input_video.mp4', save=True, conf=0.2)
+
+result = model.track('input_videos/input_video.mp4', save=True, conf=0.2)
 print(result)
 print("boxes:")
 for box in result[0].boxes:
