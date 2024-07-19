8085d274de447a3fbc7751cdfae3cc544df0350f
dawid mrotek <DawidMrotek@protonmail.ch>
2024-07-19 07:14:16 +0200

    [ADD] draw ball bounding boxes

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from utils import (read_video, 
                    save_video)
-from trackers import PlayerTracker
+from trackers import PlayerTracker, BallTracker
 
 def main():
     # Read video
     input_video_path = f"input_videos/input_video.mp4"
     output_video_path = f"output_videos/output_video.avi"
-    stub_path = f"tracker_stubs/player_detections.pkl"
 
     video_frames = read_video(input_video_path)
     
@@ -14,11 +13,19 @@ def main():
     player_tracker = PlayerTracker(model_path='yolov8x')
     player_detections = player_tracker.detect_frames(frames=video_frames,
                                                      read_from_stub=True,
-                                                     stub_path=stub_path)
+                                                     stub_path=f"tracker_stubs/player_detections.pkl")
+    
+    # Detect ball
+    ball_tracker = BallTracker(model_path='models/yolo5_best.pt')
+    ball_detections = ball_tracker.detect_frames(frames=video_frames,
+                                                     read_from_stub=False,
+                                                     stub_path=f"tracker_stubs/ball_detections.pkl")
 
     # vvv Draw output vvv
     # Draw player bounding boxes
     output_video_frames = player_tracker.draw_bboxes(video_frames, player_detections)
+    # Draw ball bounding boxes
+    output_video_frames = ball_tracker.draw_bboxes(video_frames, ball_detections)
 
 
     save_video(output_video_frames, output_video_path)
